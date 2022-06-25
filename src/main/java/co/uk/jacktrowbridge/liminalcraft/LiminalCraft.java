52712f24f9eba7db88d671f53ef7b1487f5e5559
Jack .T <jacktrowbridge2004@gmail.com>
2022-06-25 11:53:57 +0100

    Implemented dev area and player log in

--- src/main/java/co/uk/jacktrowbridge/liminalcraft/LiminalCraft.java
+++ src/main/java/co/uk/jacktrowbridge/liminalcraft/LiminalCraft.java
@@ -1,6 +1,14 @@
 package co.uk.jacktrowbridge.liminalcraft;
 
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Point;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.block.Block;
 
 public class LiminalCraft {
 
@@ -8,6 +16,29 @@ public class LiminalCraft {
 
         // Initialize
         MinecraftServer minecraftServer = MinecraftServer.init();
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+
+        // Create instance
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        instanceContainer.setGenerator(unit -> {
+            final Point start = unit.absoluteStart();
+            final Point size = unit.size();
+            for (int x = 0; x < size.blockX(); x++) {
+                for (int z = 0; z < size.blockZ(); z++) {
+                    for (int y = 0; y < Math.min(40 - start.blockY(), size.blockY()); y++) {
+                        unit.modifier().setBlock(start.add(x, y, z), Block.STONE);
+                    }
+                }
+            }
+        });
+
+        // Player
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+           final Player player = event.getPlayer();
+           event.setSpawningInstance(instanceContainer);
+           player.setRespawnPoint(new Pos(0, 42, 0));
+        });
 
         // Start the server
         minecraftServer.start("0.0.0.0", 25565);
