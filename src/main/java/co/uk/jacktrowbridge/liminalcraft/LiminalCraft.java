1fec17e9593c9ba0d737c1b3fe80c8f70b8389db
Jack .T <jacktrowbridge2004@gmail.com>
2022-06-25 13:25:31 +0100

    Implemented player skins

--- src/main/java/co/uk/jacktrowbridge/liminalcraft/LiminalCraft.java
+++ src/main/java/co/uk/jacktrowbridge/liminalcraft/LiminalCraft.java
@@ -1,23 +1,37 @@
 package co.uk.jacktrowbridge.liminalcraft;
 
 import co.uk.jacktrowbridge.liminalcraft.events.BlockProtectionEvents;
+import co.uk.jacktrowbridge.liminalcraft.utils.PlayerUtils;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Point;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
+import net.minestom.server.entity.PlayerSkin;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.network.ConnectionManager;
+import org.bson.Document;
 
 public class LiminalCraft {
 
     public static void main(String[] args){
 
+        // Database
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase database = mongoClient.getDatabase("LiminalCraft");
+        // Player database
+        MongoCollection<Document> collection = database.getCollection("players");
+
         // Initialize
         MinecraftServer minecraftServer = MinecraftServer.init();
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        ConnectionManager connectionManager = MinecraftServer.getConnectionManager();
 
         // Create instance
         InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
@@ -36,10 +50,25 @@ public class LiminalCraft {
         // Player
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+
            final Player player = event.getPlayer();
            event.setSpawningInstance(instanceContainer);
            player.setRespawnPoint(new Pos(0, 42, 0));
+
+            try {
+
+                // "https://api.mojang.com/users/profiles/minecraft/" + player.getUsername()
+
+                PlayerSkin playerSkin = PlayerSkin.fromUuid(PlayerUtils.getUUID(player.getUsername()));
+                player.setSkin(playerSkin);
+
+            } catch (Exception e) {
+                player.kick("§cWe couldn't register your account. Please try and reconnect.");
+                throw new RuntimeException(e);
+            }
+
         });
+        // TODO: Fix "Packet class net.minestom.server.network.packet.client.play.ClientSetRecipeBookStatePacket does not have any default listener!"
 
         // Registering
         registerEvents(globalEventHandler);
---
+++ src/main/java/co/uk/jacktrowbridge/liminalcraft/utils/PlayerUtils.java
@@ -0,0 +1,26 @@
+package co.uk.jacktrowbridge.liminalcraft.utils;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class PlayerUtils {
+
+    public static String getUUID(String name) {
+        String uuid = "";
+        try {
+            var in = new BufferedReader(new InputStreamReader(new URL("https://api.mojang.com/users/profiles/minecraft/" + name).openStream()));
+            uuid = (((JsonObject)new JsonParser().parse(in)).get("id")).toString().replaceAll("\"", "");
+            uuid = uuid.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
+            in.close();
+        } catch (Exception e) {
+            System.out.println("Unable to get UUID of: " + name + "!");
+            uuid = "er";
+        }
+        return uuid;
+    }
+
+}
